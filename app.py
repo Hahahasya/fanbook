27f019e18a98253e3823dc11300f4cc8e6e1e0d0
Hahahasya <muhammadzhafar437@gmail.com>
2024-06-28 15:57:07 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import os
-from os.path import join, dirname
+from os.path import join,dirname
 from dotenv import load_dotenv
-
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient 
-dotenv_path = join(dirname(__file__), 'env')
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
 load_dotenv (dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URI`")
-DB_NAME = os.environ.get("DB_NAME")
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
 app = Flask(__name__)
-
 @app.route('/')
 def home():
    return render_template('index.html')
@@ -37,3 +36,4 @@ def homework_get():
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
+   
